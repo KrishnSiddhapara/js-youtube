9bca713e69a6d5778b31670d522e3573826508bd
KrishnSiddhapara <siddhaparakishan@gmail.com>
2025-07-04 12:48:49 +0000

    02and03

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";// Treat all JS code as a newer version
+
+//alert( 3+3 ) // we are using node js , not a browser
+
+
+console.log(3+3);// code readability should be high
+
+console.log("krishn")
+
+let name = "krish"
+let age = 19
+let isLoggedIn = false
+
+
+//number  => 2 to the power 52
+//bigInt
+//boolean => true/false
+//sring => ""
+//null => standalone value
+//undefined => 
+//symbol => used to find unique
+
+//object
+
+//console.log(typeof null)
+//console.log(typeof "krishn")
+console.log(typeof undefined)
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = false
+
+// console.log(typeof score)
+// console.log(typeof(score))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+//"33" => 33
+//"33abc" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = "Krishn"
+
+let BooleanIsLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanIsLoggedIn);
+
+// 1 =>true ; 0 =>false
+
+//"" => false
+//"krishn" => true
+
+let someNumber = 32
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
