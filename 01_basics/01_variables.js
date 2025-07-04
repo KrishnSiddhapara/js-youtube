39b03d5c73a9b0cb64ab22c7af86255fedd104c4
KrishnSiddhapara <siddhaparakishan@gmail.com>
2025-07-04 02:06:42 +0000

    data types and variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId = 13322
+let accountEmail = "krishna@google.com"
+var accountPassword = "12345"
+accountCity = "dungarpur"
+let accountState;
+
+// accountId = 22 )not allowed)
+
+console.log(accountId);
+
+accountEmail = "krish@google.com"
+accountPassword  = "2121"
+accountCity = "jaipur"
+
+console.table([accountEmail , accountId , accountPassword , accountCity  , accountState])
