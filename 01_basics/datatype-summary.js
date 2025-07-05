70025bce3d1c233aa4be8122d68d102507776d4a
KrishnSiddhapara <siddhaparakishan@gmail.com>
2025-07-05 17:23:30 +0000

    datatypes-summary

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,47 @@
+// primitive
+
+// 7 types : string , Number , boolean , null , undefined , symbol , bigInt
+
+
+//Reference (Non-Premitive)
+
+//functions , objects , arrays
+
+const score = 100
+const scoreValue = 100.4
+
+const IsLoggedIn = false 
+
+const outsideTemp = null 
+
+
+
+let userEmail;  //undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+//console.log(id === anotherId);
+
+
+ const BigNumber = 212121211113333n
+
+
+ const heros = ["shaktiman",  "naagraj" , "doga"]
+
+
+ let myObj ={
+
+    name: "Krishn",
+    age : 22
+ }
+
+//  console.log(myObj)
+
+const myFunction = function(){
+
+    console.log("hello world");
+}
+
+
+console.log(typeof anotherId)
