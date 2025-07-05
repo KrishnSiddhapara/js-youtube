c9f226f150fdfcd2ea743897f5f1c3f4c78af234
KrishnSiddhapara <siddhaparakishan@gmail.com>
2025-07-05 13:11:31 +0000

    conversions

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,7 +16,7 @@ let isLoggedIn = "Krishn"
 
 let BooleanIsLoggedIn = Boolean(isLoggedIn)
 
-console.log(BooleanIsLoggedIn);
+// console.log(BooleanIsLoggedIn);
 
 // 1 =>true ; 0 =>false
 
@@ -26,5 +26,52 @@ console.log(BooleanIsLoggedIn);
 let someNumber = 32
 
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber);
+// console.log(stringNumber)
+// console.log(typeof stringNumber);
+
+
+//********************* OPERATIONS *******************************************
+
+
+
+
+let value = 5
+
+let NegValue = -value
+// console.log(NegValue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Krishn"
+
+let str3 = str1 + str2
+
+// console.log(str3)
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 8)
+
+// console.log(+true)
+
+let num1 , num2 , num3
+
+num1 = num2 = num3 = 3+3
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter)
+
+
